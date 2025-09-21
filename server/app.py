4ca582867df8ceba5ea8cb2520e8cd5b4a77a0cf
Felix Kemboi <felixkemboi824@gmail.com>
2025-09-21 22:56:52 +0300

    all the tests passed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -17,11 +18,12 @@ db.init_app(app)
 api = Api(app)
 
 
+# ---------------- ALL PLANTS ----------------
 class Plants(Resource):
 
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return make_response(plants, 200)
 
     def post(self):
         data = request.get_json()
@@ -41,15 +43,48 @@ class Plants(Resource):
 api.add_resource(Plants, '/plants')
 
 
+# ---------------- SINGLE PLANT ----------------
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        return make_response(plant.to_dict(), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        data = request.get_json()
+
+        if "name" in data:
+            plant.name = data["name"]
+        if "image" in data:
+            plant.image = data["image"]
+        if "price" in data:
+            plant.price = data["price"]
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
+# ---------------- MAIN ENTRY ----------------
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
